48e812e42fdf3ada2d52117597edc6d0fb00d74f
seth <rader.seth@gmail.com>
2022-04-06 20:29:25 -0400

    Add test for parser IO errors

--- src/qmap/parser_test.rs
+++ src/qmap/parser_test.rs
@@ -1,5 +1,20 @@
 use crate::qmap;
 use qmap::parser::parse;
+use std::io;
+
+struct ErroringReader {}
+
+impl ErroringReader {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl io::Read for ErroringReader {
+    fn read(&mut self, _: &mut [u8]) -> io::Result<usize> {
+        Err(io::Error::new(io::ErrorKind::Other, "Generic test error"))
+    }
+}
 
 // Parse successes
 
@@ -15,6 +30,16 @@ fn parse_empty_map() {
 fn parse_token_error() {
     let err = parse(&b"\""[..]).err().unwrap();
     if let qmap::result::Error::Lexer(_) = err {
+    } else {
+        panic!("Unexpected error variant for {}", err);
+    }
+}
+
+#[test]
+fn parse_io_error() {
+    let reader = ErroringReader::new();
+    let err = parse(reader).err().unwrap();
+    if let qmap::result::Error::Io(_) = err {
     } else {
         panic!("Unexpected error variant for {}", err);
     }
