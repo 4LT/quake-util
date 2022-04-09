bfb7a1773d23845c5c8a92fc3e62c66c041634f7
seth <rader.seth@gmail.com>
2022-04-08 21:10:58 -0400

    Require alloc_fills instead of hashbrown when not building w/ feature 'std'

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
-#[cfg(all(not(feature = "std"), not(feature = "hashbrown")))]
-compile_error!("Must use feature 'std' or include 'hashbrown'");
+#[cfg(all(not(feature = "std"), not(feature = "alloc_fills")))]
+compile_error!("Must use feature 'std' or include 'alloc_fills'");
 
 #[cfg(feature = "std")]
 #[macro_use]
